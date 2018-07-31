bb15557ba9d570182cd6fdd9e4e5bff3997e13f6
Jonathan Nunes <41246714+MrWigglyTiggly@users.noreply.github.com>
2018-07-31 13:41:28 -0400

    Add files via upload

---
+++ program.py
@@ -0,0 +1,30 @@
+import random
+
+print('---------------------------------')
+print('    GUESS THAT NUMBER GAME ')
+print('---------------------------------')
+print()
+
+
+
+the_number = random.randint(0,100)
+guess = -1
+
+name = input('What is your name? ')
+
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
+
+    if guess < the_number:
+        print('Sorry {}, the number {} is too LOW'.format(name,guess))
+    elif guess > the_number:
+        print('Sorry {}, the number {} is too HIGH'.format(name,guess))
+    else:
+        print('Congatulations {}, the number {} was correct'.format(name,guess))
+
+
+print('done')
+
+#print(guess_text, type(guess_text))
+#print(guess, type(guess))
